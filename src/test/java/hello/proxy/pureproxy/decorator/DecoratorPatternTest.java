e3a0fc5fca44b109e951cb9dbdabf2a28170615f
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-10-03 00:40:28 +0900

    Decorator Pattern test 적용

---
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,31 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.*;
+import org.junit.jupiter.api.Test;
+
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecorator() {
+        Component component = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(component);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component component = new RealComponent();
+        Component decorator = new MessageDecorator(component);
+        DecoratorPatternClient client = new DecoratorPatternClient(decorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component component = new RealComponent();
+        Component messageDecorator = new MessageDecorator(component);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+}
